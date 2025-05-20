8645ee3639f1efebb5198058f7027656c4b1174f
Abdelrahman El Sharabasy <boodysharabas@gmail.com>
2025-05-20 06:42:03 +0300

    Valid Anagram

---
+++ Easy/Valid Anagram.js
@@ -0,0 +1,9 @@
+"use strict";
+function isAnagram(s, t) {
+    if (s.length !== t.length)
+        return false;
+    const w1 = s.split("").sort().join("");
+    const w2 = t.split("").sort().join("");
+    return (w1 === w2 ? true : false);
+}
+;
